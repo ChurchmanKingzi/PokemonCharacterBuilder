03cf0b01651865d3f386371c782dc61d78fc5b54
ChurchmanKingzi <119212806+ChurchmanKingzi@users.noreply.github.com>
2025-04-19 12:16:30 +0200

    Add files via upload

--- vorteilService.js
+++ vorteilService.js
@@ -3,6 +3,16 @@ const vorteilService = {
     getAllVorteile: function() {
         return [
             {
+                id: 'adrenalinschub',
+                name: 'Adrenalinschub',
+                beschreibung: 'Du arbeitest am besten unter Druck. Immer, wenn du dich in einer Extremsituation befindest (wenn deine KP unter 25% fallen, du in einer scheinbar ausweglosen Situation bist oder ein Gruppenmitglied (ein Mensch) bewusstlos wird oder stirbt), wird dein Geist klarer und du erhältst vorübergehend einen zusätzlichen Erfolg auf alle Proben, die du machst. Im Kampf kannst du in diesem Zustand deine Aktion nutzen, um dein Pokemon anzufeuern, wodurch es einen zusätzlichen Erfolg bei seiner nächsten GENA-Probe bekommt. Du kannst diesen Zustand NICHT absichtlich herbeiführen, indem du z.B. deinen eigenen Kameraden ausknockst. Er passiert nur in wirklich stressigen Notsituationen. Wenn das Adrenalin abklingt, wirst du für eine Stunde extrem erschöpft und wobbelig auf den Beinen.'
+            },
+            {
+                id: 'analyst',
+                name: 'Analyst',
+                beschreibung: 'Wenn du das erste Mal gegen ein Pokemon einer Spezies kämpfst, analysierst du es sehr genau. Der Kampf muss mindestens 2 volle Runden dauern, du musst ihn durchgehend sehen können und dein Pokemon muss aktiv daran beteiligt sein, damit deine Analyse erfolgreich ist. Nach erfolgreicher Analyse kannst du zukünftig besser gegen Pokemon dieser Spezies kämpfen. Alle GENA- und PA-Proben von dir oder deinen Pokemon gegen Pokemon der Spezies erhalten 3 zusätzliche Würfel. Du kannst theoretisch unendlich viele Pokemon analysiert haben (musst dir aber natürlich irgendwo notieren, welche du kennst).'
+            },
+            {
                 id: 'besonders-gluecklich',
                 name: 'Besonders Glücklich',
                 beschreibung: 'Wenn du eine Probe mit einem Glücks-Token wiederholst: Du erhältst +2 Erfolge.'
@@ -16,6 +26,11 @@ const vorteilService = {
                 id: 'connections-zu-kurt',
                 name: 'Connections zu Kurt',
                 beschreibung: 'Du kennst Kurt, den Pokeball-Bauer, persönlich und hast enge Beziehungen zu ihm; vielleicht freundschaftlich, vielleicht schuldet er dir einfach was. In jedem Fall kannst du ihn einmal pro Woche anrufen und er wird dir einen seiner handgefertigten Pokebälle schicken. Du kannst auf Überreden oder Überzeugen würfeln, um dir einen auszusuchen, sonst ist es ein zufälliger von folgenden Optionen: KÖDERBALL (+2 automatische Erfolge bei Fangen-Proben gegen geangelte Pokemon), MONDBALL (+3 Würfel für Fangen-Proben bei Nacht), FREUNDESBALL (macht Pokemon, die er fängt, sofort zutraulich und gibt +1W6 Freundschaft), SYMPABALL (+3 Würfel für Fangen-Proben, wenn dein aktives Pokemon ein anderes Geschlecht hat als das zu fangende), LEVELBALL (+1 automatischer Erfolg für Fangen-Proben pro 10 volle Level, die dein aktives Pokemon höher ist als das Ziel), SCHWERBALL (+1 automatischer Erfolg pro 150 Kilo, die das Pokemon wiegt, max 3), TURBOBALL (+1 automatischer Erfolg pro 10 volle INIT-Base-Stat-Punkte, die das Ziel über 100 hat (max 3).)'
+            },
+            {
+                id: 'critical_capture',
+                name: 'Critical Capture',
+                beschreibung: 'Fangen-Proben sind für dich um eine Stufe erleichtert. Wenn du ein Pokemon fängst, erhält es pro Erfolg bei seiner Fangen-Probe entweder +1 Freundschaft oder +1 Level (deine Wahl, beliebige Kombinationen möglich). Ist die Fangen-Probe ein kritischer Erfolg (4+ Erfolge), erhält das Pokemon außerdem +1 GENA und +1 PA.'
             },
             {
                 id: 'critter',
@@ -51,6 +66,11 @@ const vorteilService = {
                 id: 'glueckspilz',
                 name: 'Glückspilz',
                 beschreibung: 'Du erhältst +5 maximale Glücks-Tokens.'
+            },
+            {
+                id: 'guter_ruf',
+                name: 'Guter Ruf',
+                beschreibung: 'Du hast dir bereits einen guten Ruf aufgebaut, z.B. durch eine Heldentat oder bemerkenswerte Leistungen. Wer schon von dir gehört hat, hängt von der Art deiner Tat oder Leistung ab. Wer deinen Namen kennt, wird dir mit größerer Wahrscheinlichkeit positiv gesinnt sein. Proben auf Anführen, Beeindrucken, Betören, Feilschen und Überreden sind bei solchen Personen um eine Stufe erleichtert. Es könnte dir, wenn du deine Karten richtig spielst, auch leicht möglich sein, Gefälligkeiten von solchen Zielen zu bekommen. Du kannst dir diesen Ruf während des Spiels ruinieren, indem du z.B. kriminelle Dinge tust oder dich feige verhältst.'
             },
             {
                 id: 'heilende-hand',
@@ -90,7 +110,7 @@ const vorteilService = {
             {
                 id: 'medizinische-grundausbildung',
                 name: 'Medizinische Grundausbildung',
-                beschreibung: 'Mit entsprechendem Werkzeug(!) kannst du Erste Hilfe nutzen, um Wunden zu versorgen. Die Wunden sind dann zwar nicht einfach weg (der gebrochene Arm ist z.B. geschient, aber immer noch gebrochen), aber regeltechnisch bringen sie keine Wunde mehr ein. Normalerweise kann Erste Hilfe nur KP regenerieren und Bewusstlosigkeit aufheben, aber keine Wunden entfernen.'
+                beschreibung: 'Mit entsprechendem Werkzeug(!) kannst du Erste Hilfe nutzen, um Wunden zu versorgen. Die Wunden sind dann zwar nicht einfach weg (der gebrochene Arm ist z.B. geschient, aber immer noch gebrochen), aber regeltechnisch bringen sie keine Wunde mehr ein. Normalerweise kann Erste Hilfe nur KP regenerieren und Bewusstlosigkeit aufheben, aber keine Wunden entfernen. Außerdem kannst du, wenn du Heilkräuter und ein Labor zur Verfügung hast, Tränke, Supertränke und andere Items herstellen.'
             },
             {
                 id: 'nachtsicht',
@@ -101,6 +121,11 @@ const vorteilService = {
                 id: 'pokemon-freund',
                 name: 'Pokemon-Freund',
                 beschreibung: 'Wilde Pokemon betrachten dich von Natur aus als freundlich und werden dich (in der Regel) nie von sich aus angreifen. Sie werden sich aber natürlich verteidigen, wenn du sie (oder ein Pokemon in ihrer Nähe) angreifst. Wenn du zu grausam oder blutrünstig bist, verlierst du diesen Vorteil irgendwann.'
+            },
+            {
+                id: 'pokemon-fluesterer',
+                name: 'Pokemon-Flüsterer',
+                beschreibung: 'Du kannst it deinen Pokemon auf einer tieferen Ebene connecten als andere Trainer. Proben auf Umgang mit Pokemon mit deinen Pokemon erhalten zwei zusätzliche Erfolge und deine Pokemon generieren immer doppelt so viel Freundschaft, wie sie es sonst täten.'
             },
             {
                 id: 'psycho-sicht',
@@ -141,6 +166,11 @@ const vorteilService = {
                 id: 'typ-experte',
                 name: 'Typ-Experte',
                 beschreibung: 'Wähle einen Typ. Du erhältst +1 Erfolge für alle Proben auf Umgang mit Pokemon und Fangen für Pokemon dieses Typs, aber +1 Patzer für alle Proben auf Umgang mit Pokemon und Fangen für Pokemon, die nicht diesen Typ haben. Wenn du ein Pokemon des Typs fängst, erhält es wegen deines guten Umgangs automatisch +1 GENA und +1 PA. Deine Typ-Meisterschaft mit diesem Typ steigt doppelt so schnell.'
+            },
+            {
+                id: 'typ-resistenz',
+                name: 'Typ-Resistenz',
+                beschreibung: 'Wähle einen Typ. Du hast eine natürliche Resistenz gegen diesen Typ und nimmst nur halben Schaden von seinen Attacken. Pokemon des Typs erkennen in dir instinktiv eine größere Gefahr/einen stärkeren Gegner als in normalen Menschen.'
             },
             {
                 id: 'vernarbt',
